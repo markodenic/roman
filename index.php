1a8db23984359b4ab964ba08703174bfc1ed910c
marko <marko.denic84@gmail.com>
2017-04-12 08:17:55 +0200

    Simple preview added

--- index.php
+++ index.php
@@ -4,4 +4,23 @@ require 'vendor/autoload.php';
 
 $roman = new \Acme\RomanNumeralConverter();
 
-echo $roman->convert(291);
+?>
+
+
+    <form action="" method="post">
+
+        <label for="number">Regular Number</label>
+        <input type="text" name="number" id="number">
+
+    </form>
+
+
+<?php
+
+if ( ! empty($_POST)):
+
+    echo 'Roman number: ' . $roman->convert($_POST['number']);
+
+endif;
+
+?>
